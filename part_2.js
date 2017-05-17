542b7bea639d57a2dcd732b5af00a30424f34972
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-17 11:44:44 +0530

    Update part-2 use array reduce method

--- part_2.js
+++ part_2.js
@@ -9,49 +9,31 @@ const UPDATE_NOTE = "UPDATE_NOTE";
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case ADD_NOTE:
-      /********** Using object spread operator **********/
-      // return {
-      //   ...state,
-      //   nextNoteId: state.nextNoteId + 1,
-      //   notes: {
-      //     ...state.notes,
-      //     action.note.id: action.note
-      //   }
-      // };
       return Object.assign({}, state, { nextNoteId: state.nextNoteId + 1 }, { notes: Object.assign({}, state.notes, { [action.note.id]: action.note }) });
     case UPDATE_NOTE:
-      /********** Using object spread operator **********/
-      // return {
-      //   ...state,
-      //   notes: {
-      //     ...state.notes,
-      //     action.note.id: action.note
-      //   }
-      // };
       return Object.assign({}, state, { notes: Object.assign({}, state.notes, { [action.note.id]: action.note }) });
     default:
       return state;
   }
 };
 
-const action1 = {
-  type: ADD_NOTE,
-  note: {
-    id: 1,
-    content: "Hello world!"
+const actions = [
+  {
+    type: ADD_NOTE,
+    note: {
+      id: 1,
+      content: "Hello world!"
+    }
+  },
+  {
+    type: UPDATE_NOTE,
+    note: {
+      id: 1,
+      content: "Hello Garima!"
+    }
   }
-};
+];
 
-const action2 = {
-  type: UPDATE_NOTE,
-  note: {
-    id: 1,
-    content: "Hello Garima!"
-  }
-};
+const state = actions.reduce(reducer, undefined);
 
-const state0 = reducer(undefined, action1);
-const state1 = reducer(state0, action2);
-
-console.log('This is state0', JSON.stringify(state0, null, 2));
-console.log('This is state1', JSON.stringify(state1, null, 2));
+console.log('This is the final state', JSON.stringify(state, null, 2));
