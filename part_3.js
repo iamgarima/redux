e2b735a3ed73890ea6aa4d7e20fdcc5a07dc4df1
Garima Kamboj <garima.kamboj@icloud.com>
2017-05-17 11:45:41 +0530

    Add part-3 add 'validateAction' and 'createStore' functions to create store to use it for updating and getting state

---
+++ part_3.js
@@ -0,0 +1,70 @@
+const initialState = {
+  nextNoteId: 1,
+  notes: {}
+};
+
+const ADD_NOTE = "ADD_NOTE";
+const UPDATE_NOTE = "UPDATE_NOTE";
+
+const actions = [
+  {
+    type: ADD_NOTE,
+    note: {
+      id: 1,
+      content: "Hello world!"
+    }
+  },
+  {
+    type: ADD_NOTE,
+    note: {
+      id: 2,
+      content: "Hello!"
+    }
+  },
+  {
+    type: UPDATE_NOTE,
+    note: {
+      id: 1,
+      content: "Hello Garima!"
+    }
+  }
+];
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_NOTE:
+      return Object.assign({}, state, { nextNoteId: state.nextNoteId + 1 }, { notes: Object.assign({}, state.notes, { [action.note.id]: action.note }) });
+    case UPDATE_NOTE:
+      return Object.assign({}, state, { notes: Object.assign({}, state.notes, { [action.note.id]: action.note }) });
+    default:
+      return state;
+  }
+};
+
+const validateAction = action => {
+  if(!action || typeof action !== 'object' || Array.isArray(action)) {
+    throw new Error('Action must be an object!');
+  }
+  if(typeof action.type === 'undefined') {
+    throw new Error('Action must have a type!');
+  }
+};
+
+const createStore = reducer => {
+  let state = undefined;
+  return {
+    dispatch: (action) => {
+      validateAction(action);
+      state = reducer(state, action);
+    },
+    getState: () => state
+  }
+};
+
+const store = createStore(reducer);
+
+store.dispatch(actions[0]);
+console.log(store.getState());
+
+store.dispatch(actions[1]);
+console.log(store.getState());
